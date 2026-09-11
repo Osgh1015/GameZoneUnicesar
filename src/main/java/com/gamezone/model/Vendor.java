be33ef3a7f0942ab806e262c8c3cfaa83f7508b0
PAMAYA22 <pamaya@unicesar.edu.co>
2026-09-10 23:56:14 -0500

    docs: add JavaDoc to Vendor class

--- src/main/java/com/gamezone/model/Vendor.java
+++ src/main/java/com/gamezone/model/Vendor.java
@@ -1,12 +1,28 @@
 package com.gamezone.model;
 
-
+/**
+ * Represents a vendor (employee) of the store, in charge of attending
+ * clients and registering sales.
+ * <p>
+ * In addition to the attributes shared with every person, a Vendor has
+ * an employee code and an assigned work shift. Vendors are preloaded by
+ * the team (they are already hired staff) instead of being registered
+ * through the user interface.
+ */
 public class Vendor extends Person {
 
     private String employeeCode;
     private String workShift;
 
-
+    /**
+     * Creates a new vendor.
+     *
+     * @param id           unique identification of the vendor
+     * @param name         full name of the vendor
+     * @param phone        contact phone number
+     * @param employeeCode internal employee code
+     * @param workShift    assigned work shift (e.g. "Morning", "Afternoon")
+     */
     public Vendor(String id, String name, String phone, String employeeCode, String workShift) {
         super(id, name, phone);
         this.employeeCode = employeeCode;
@@ -29,7 +45,9 @@ public class Vendor extends Person {
         this.workShift = workShift;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String describe() {
         return "Vendor: " + getName() + " (Employee code: " + employeeCode
