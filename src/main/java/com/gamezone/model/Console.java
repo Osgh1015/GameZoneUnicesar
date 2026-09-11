f3f7b4e557bf3ebb7ead487c4f7b2957bece7629
Sandres-Herrera <sandresherrera@unicesar.edu.co>
2026-09-10 19:52:35 -0500

    feat: implement Console getters, setters and description override

--- src/main/java/com/gamezone/model/Console.java
+++ src/main/java/com/gamezone/model/Console.java
@@ -17,8 +17,34 @@ public class Console extends Product {
         this.generation = generation;
     }
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
     @Override
     public String getDescription() {
-        return null;
+        return String.format(
+                "Console: %s | Brand: %s | Model: %s | Generation: %d | Price: %.2f | Stock: %d",
+                getTitle(), brand, model, generation, getPrice(), getQuantity());
     }
 }
